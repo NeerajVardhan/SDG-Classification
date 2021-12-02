f4c2c31462322aebfe1aa4fd260d7d286f05cae9
NeerajVardhan <56908185+NeerajVardhan@users.noreply.github.com>
2021-12-02 23:30:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def classify(a):
             ,"Reduced Inequalites","Sustainable Cities and Communities",
             "Responsible Consumption and Production","Climate Action","Life Below Water","Life On Land"]
     t =zip(li_goals,test_res)
-    df_predic = pd.DataFrame(t,columns=["SDG","Probability"])
+    df_predic = pd.DataFrame(t,columns=["Goals","Probability"])
     df_predic.index = df_predic.index + 1
     return((df_predic))
     
@@ -35,7 +35,7 @@ def classify(a):
 
 with header:
     titl, imga = st.beta_columns(2)
-    st.title('Sustainable Development Goal Classification') 
+    st.title('SGD Classification') 
      
 
 with body:
@@ -46,14 +46,14 @@ with body:
         'Choose your .txt file', type="txt")
     if uploaded_file is not None:
         rawtext = str(uploaded_file.read(), 'utf-8')
-    if st.button('Get Results'):
+    if st.button('Get Classified Output'):
         with classify_container:
             if rawtext == "":
                 st.header('Results')
                 st.write('Please enter text or upload the file')
             else:
                 result = classify(rawtext)
-                st.header('Sdg Classification Results')
+                st.header('SDG Classification Results')
                 st.dataframe(result)
                 df = pd.DataFrame(result, columns = ["Probability"])
                 st.line_chart(df)            
